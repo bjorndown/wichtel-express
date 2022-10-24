85f50c75c9c56764d41ce655348c8708db619a16
Björn Mosler <dev@bjoern.mosler.ch>
2022-10-24 21:59:32 +0200

    Remove title tag in _document.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="de">
       <Head>
-        <title>wichtel.express</title>
         <meta
           name="description"
           content="Wichtel-Auslosung ohne Anmeldung &ndash; schnell und unkompliziert."
